ec27423ab3a468946faed79839076c5bc4d24264
stephenkridel <stephen.kridel@gmail.com>
2022-05-25 03:38:46 +0000

    Added routing that allow you to query the database for first names. Uninstalled nginx and reinstalled. Setup nginx as a reverse proxy to point to localhost:3000 and serve the content index.js requests.

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
+
 async function insertData(name, occupation) {
-	await pool.query(
-		"INSERT INTO therapists (name, occupation) VALUES ($1, $2)",
-		[name, occupation]
-	);
+	try {
+		await pool.query(
+			"INSERT INTO therapists (name, occupation) VALUES ($1, $2)",
+			[name, occupation]
+		);
+	} catch (error) {
+		throw(error);
+	}
 }
 
 async function getData() {
@@ -26,14 +38,19 @@ app.get('/', (req, res) => {
 	res.json({ response: 'This is the root' });
 });
 
-app.get('/query/:name-:occupation', async (req, res) => {
+app.get('/query', async (req, res) => {
 	const data = await getData();
+	res.send(data);
+});
+
+app.get('/query/:name', async (req, res) => {
+	const data = await pool.query('SELECT * FROM therapists WHERE name=$1', [req.params.name]);
 	res.send(data);
 });
 
 app.post('/query', async (req, res) => {
 	await insertData(req.params.name, req.params.occupation);
-	res.send('Inserted therapist into data base');
+	res.send('Inserted therapist into database');
 });
 
 app.listen(PORT, () => {
